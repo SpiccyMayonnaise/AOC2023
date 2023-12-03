f8cd82e2057cb6aee121635dc81c23d96f20296f
valen <valen.kostic@gmail.com>
2023-12-03 23:38:33 +1030

    Day 1 part 2

--- src/day1.cpp
+++ src/day1.cpp
@@ -1,7 +1,55 @@
 #include <iostream>
 #include <fstream>
 #include <cstdint>
+#include <array>
 #include "utils.h"
+
+uint32_t findFirstDigit(const std::string& source, bool forwards) {
+	std::array<std::string, 9> digits = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+	std::array<std::string, 9> words = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+	size_t bestIndex = std::string::npos;
+	uint32_t bestDigit = 0;
+
+	for (size_t i = 0; i < digits.size(); ++i) {
+		auto pos = forwards ? source.find(digits[i]) : source.rfind(digits[i]);
+
+		if (pos != std::string::npos) {
+			if (bestIndex == std::string::npos) {
+				bestIndex = pos;
+				bestDigit = i;
+			}
+
+			if ((forwards && pos < bestIndex) || (!forwards && pos > bestIndex)) {
+				bestIndex = pos;
+				bestDigit = i;
+			}
+		}
+	}
+	for (size_t i = 0; i < words.size(); ++i) {
+		auto pos = forwards ? source.find(words[i]) : source.rfind(words[i]);
+
+		if (pos != std::string::npos) {
+			if (!forwards)
+				pos += words[i].length() - 1;
+
+			if (bestIndex == std::string::npos) {
+				bestIndex = pos;
+				bestDigit = i;
+			}
+
+			if ((forwards && pos < bestIndex) || (!forwards && pos > bestIndex)) {
+				bestIndex = pos;
+				bestDigit = i;
+			}
+		}
+	}
+
+	if (bestIndex == std::string::npos)
+		return 0;
+
+	return bestDigit + 1;
+}
 
 int main() {
 	std::ifstream inputStream{"./inputs/day1.txt", std::ios::in | std::ios::binary};
@@ -10,21 +58,13 @@ int main() {
 
 	std::string line;
 	while (std::getline(inputStream, line)) {
-		char firstDigit = '\0';
-		char lastDigit = '\0';
+		auto firstDigit = findFirstDigit(line, true);
+		auto lastDigit = findFirstDigit(line, false);
 
-		for (const char& c : line) {
-			if (c >= '0' && c <= '9') {
-				if (firstDigit == '\0')
-					firstDigit = c;
-				lastDigit = c;
-			}
-		}
-
-		uint32_t num = (firstDigit - '0')*10 + lastDigit - '0';
-		sum += num;
-
+		auto num = firstDigit * 10 + lastDigit;
 		std::cout << num << std::endl;
+
+		sum += num;
 	}
 
 	std::cout << sum << std::endl;
